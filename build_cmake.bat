9721b493b97f3d903f0761365752756a31fa72bf
lollolong <65832054+lollolong@users.noreply.github.com>
2024-05-27 20:13:59 +0200

    update build script

--- build_cmake.bat
+++ build_cmake.bat
@@ -3,3 +3,18 @@
 mkdir bin
 cd bin
 cmake ..
+
+for /f "usebackq tokens=*" %%i in (`vswhere -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
+  set InstallDir=%%i
+)
+
+if exist "%InstallDir%\Common7\Tools\vsdevcmd.bat" (
+  call "%InstallDir%\Common7\Tools\vsdevcmd.bat"
+)
+
+devenv gxt2.sln /rebuild Debug
+devenv gxt2.sln /rebuild Release
+
+if %ERRORLEVEL% NEQ 0 (
+  pause
+)
