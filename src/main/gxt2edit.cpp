a6d2b956068076183daf44426c0cc9f19d6d1bbc
lollolong <65832054+lollolong@users.noreply.github.com>
2024-05-28 11:55:17 +0200

    change close hotkey
    conflict with text copy

--- src/main/gxt2edit.cpp
+++ src/main/gxt2edit.cpp
@@ -96,7 +96,7 @@ void gxt2edit::RenderMenuBar()
 			{
 				m_RequestOpenFile = true;
 			}
-			if (ImGui::MenuItem(ICON_FA_FOLDER_CLOSED " Close File", "CTRL + C"))
+			if (ImGui::MenuItem(ICON_FA_FOLDER_CLOSED " Close File", "CTRL + SHIFT + C"))
 			{
 				m_RequestCloseFile = true;
 			}
@@ -384,7 +384,7 @@ void gxt2edit::ProcessShortcuts()
 	{
 		m_RequestOpenFile = true;
 	}
-	if (io.KeyCtrl && ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_C)))
+	if (io.KeyCtrl && io.KeyShift && ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_C)))
 	{
 		m_RequestCloseFile = true;
 	}
