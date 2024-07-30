9153ff2a03af6e66a3c2d6d764082ee80d4d07d3
lollolong <65832054+lollolong@users.noreply.github.com>
2024-07-30 20:54:02 +0200

    imgui fetch content

---
+++ cmake/imgui.cmake
@@ -0,0 +1,29 @@
+include(FetchContent)
+
+FetchContent_Declare(
+    imgui
+    GIT_REPOSITORY https://github.com/ocornut/imgui.git
+    GIT_TAG        8199457a7d9e453f8d3d9cadc14683fb54a858b5
+    GIT_PROGRESS   TRUE
+)
+FetchContent_Populate(imgui)
+
+file(GLOB IMGUI_SOURCE_FILES
+    ${imgui_SOURCE_DIR}/*.h
+    ${imgui_SOURCE_DIR}/*.cpp
+    ${imgui_SOURCE_DIR}/misc/cpp/*.*
+    ${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.h
+    ${imgui_SOURCE_DIR}/backends/imgui_impl_glfw.cpp
+    ${imgui_SOURCE_DIR}/backends/imgui_impl_vulkan.h
+    ${imgui_SOURCE_DIR}/backends/imgui_impl_vulkan.cpp
+)
+
+add_library(imgui STATIC ${IMGUI_SOURCE_FILES})
+
+target_include_directories(imgui PUBLIC 
+	${imgui_SOURCE_DIR}
+	${imgui_SOURCE_DIR}/backends/
+)
+
+target_link_libraries(imgui PUBLIC glfw ${Vulkan_LIBRARIES})
+target_include_directories(imgui PUBLIC ${Vulkan_INCLUDE_DIRS})
