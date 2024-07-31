45472ed9da0ad736497c0ce1b6390edd6ee5f281
lollolong <65832054+lollolong@users.noreply.github.com>
2024-07-31 10:13:30 +0200

    glfw unity

--- cmake/glfw.cmake
+++ cmake/glfw.cmake
@@ -15,3 +15,8 @@ FetchContent_MakeAvailable(glfw)
 
 set_target_properties(glfw PROPERTIES FOLDER "Dependencies")
 set_target_properties(update_mappings PROPERTIES FOLDER "Dependencies")
+
+if(GXT2_ENABLE_UNITY_BUILD)
+	set_target_properties(glfw PROPERTIES UNITY_BUILD ON)
+	set_target_properties(glfw PROPERTIES UNITY_BUILD_BATCH_SIZE 16)
+endif(GXT2_ENABLE_UNITY_BUILD)
